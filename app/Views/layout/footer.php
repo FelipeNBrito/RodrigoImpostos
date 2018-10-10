9fd75da1cab0e44d65bbee358cc2d192fd5b4826
Felipe Neves Brito <felipe.brito@softexpert.com>
2018-10-10 14:44:26 -0300

    BUILD INICIAL

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -2,18 +2,18 @@
 <!-- /#wrapper -->
 
 <!-- Bootstrap Core JavaScript -->
-<script src="/assets/js/bootstrap.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/bootstrap.min.js"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="/assets/js/metisMenu.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/metisMenu.min.js"></script>
 
 <!-- Morris Charts JavaScript -->
-<script src="/assets/js/raphael.min.js"></script>
-<script src="/assets/js/morris.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/raphael.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/morris.min.js"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="/assets/js/jquery-ui.min.js"></script>
-<script src="/assets/js/sb-admin-2.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/jquery-ui.min.js"></script>
+<script src="http://ec2-52-15-180-127.us-east-2.compute.amazonaws.com/RodrigoImpostos/assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
